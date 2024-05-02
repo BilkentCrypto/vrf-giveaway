750d1381b54efa5239fe541accb31ee1f988dce1
Kaan Aydeniz <kaanayden15@gmail.com>
2024-05-02 17:32:10 +0300

    Initialize next

---
+++ website/src/app/page.js
@@ -0,0 +1,11 @@
+import { GiveawayPage } from "@/components/giveaway-page";
+import { Main } from "next/document";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <GiveawayPage />
+    </main>
+  );
+}
