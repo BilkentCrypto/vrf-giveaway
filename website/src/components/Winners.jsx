87d2cd7e7e9944546d5dd66c2ce4a10d497d8689
Kaan Aydeniz <kaanayden15@gmail.com>
2024-05-04 06:14:25 +0300

    Fix winners bugs

--- website/src/components/Winners.jsx
+++ website/src/components/Winners.jsx
@@ -14,27 +14,28 @@ export async function Winners() {
     const winners = [];
 
     const isDone = await contract.giveawayDone();
-
-    for(let i = 0; i < WINNER_NO; i++) {
-        let winnerIndex = await contract.getPrize(i)
-        let winnerHash = await contract.participants(winnerIndex);
-        const participant = await Participant.findOne({emailHash: winnerHash});
-        winners.push(participant);
+    if (isDone) {
+        for (let i = 0; i < WINNER_NO; i++) {
+            let winnerIndex = await contract.getPrize(i)
+            let winnerHash = await contract.participants(winnerIndex);
+            const participant = await Participant.findOne({ emailHash: winnerHash });
+            winners.push(participant);
+        }
     }
 
     return (<>
 
         <div className="bg-gray-800 rounded-md p-4 px-8 space-y-2 overflow-auto max-h-64">
             <h2 className="text-lg font-medium text-white text-center">Winners</h2>
-            { !isDone ?
-            <ul className="space-y-1 text-sm text-center text-white">
-                {winners.map((participant) =>
-                    <li key={participant.emailHash}>{`${participant.name} - ${participant.emailHash.toString().substring(0,20)}...`}</li>
-                )}
-            </ul>
- : 
- <p className="space-y-1 text-sm text-center text-white">Giveaway is not finished yet.</p>
- }
+            {isDone ?
+                <ul className="space-y-1 text-sm text-center text-white">
+                    {winners.map((participant) =>
+                        <li key={participant.emailHash}>{`${participant.name} - ${participant.emailHash.toString().substring(0, 20)}...`}</li>
+                    )}
+                </ul>
+                :
+                <p className="space-y-1 text-sm text-center text-white">Giveaway is not finished yet.</p>
+            }
         </div>
 
     </>);
